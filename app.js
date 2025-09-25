7234bcb2e6e36c5e429a4ed3f51aab5be5e592b4
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-09-25 10:22:07 +0700

    Nho Nhi qua

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-// dùng thư viện express js
+import express from 'express';
+import morgan from 'morgan';
+import { engine } from 'express-handlebars';
+const app = express();
 
-const express = require('express')
-const app = express()
 // mở port 3000
 const port = 3000;
 
-// route
-app.get('/',  (req, res) => {
-  return res.send('Hello World')
-})
+// mogan
 
-app.listen(port, ()=> {console.log(`Listen ${port}`)})
+app.use(morgan("combined"));
+
+// route
+//đăng ký template engine cho Express, để khi render file .handlebars, Express sẽ biết cách xử lý nó.
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+//Nói với Express rằng view engine mặc định sẽ là handlebars.
+//Khi gọi res.render('home'), Express sẽ tự tìm file home.handlebars trong thư mục views.
+app.set('views', './views');
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
